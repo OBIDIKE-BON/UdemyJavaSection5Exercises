ad7d8299fede8c3e071705ec0b7571369555ca2c
obidikejekwu <obidike.jekwu@gmail.com>
2022-07-13 17:35:09 +0100

    NumberInWord final Edit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        NumberInWord.printNumberInWord(2);
+        NumberInWord.printNumberInWord(-2);
+        NumberInWord.printNumberInWord(22);
+    }
+}
